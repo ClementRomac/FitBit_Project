0c3e9cbb0ef4f05c17c1df53767fb6af6ca0aa19
leroy <pierre.leroy@ynov.com>
2016-05-02 16:13:51 +0200

    Traitement Date_Weight

---
+++ Traitement/Date_Weight.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leroy
+ * Date: 02/05/2016
+ * Time: 16:10
+ */
+### Conversion du CSV en tableau PHP ###
+var_dump(parse_csv_file('C:\Cours_ingesup\B2\Rmoocstats\dataBorris.csv', true, ';')); /* selection du fichier */
+function parse_csv_file($file, $columnheadings = false, $delimiter = ',', $enclosure = "\"") {
+
+    $row = 1;
+    $rows = array();
+    $handle = fopen($file, 'r');
+
+    while (($data = fgetcsv($handle, 1000, $delimiter, $enclosure )) !== FALSE) {
+
+        if (!($columnheadings == false) && ($row == 1)) {
+            $headingTexts = $data;
+        } elseif (!($columnheadings == false)) {
+            foreach ($data as $key => $value) {
+                unset($data[$key]);
+                $data[$headingTexts[$key]] = $value;
+            }
+            $rows[] = $data;
+        } else {
+            $rows[] = $data;
+        }
+        $row++;
+    }
+
+    fclose($handle);
+    return $rows;
+}
+
+?>

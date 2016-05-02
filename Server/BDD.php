eb2aa9813a69396a4296da58ea7c67f838c63f64
Cédric Prezelin <cedric.prezelin@outlook.com>
2016-05-02 16:04:15 +0200

    renommage bdd

--- Server/BDD.php
+++ Server/BDD.php
@@ -1,11 +1,12 @@
-BDD : https://phpmyadmin.alwaysdata.com/
-
-User : 122417_root
-
-Mot de Passe : password
-
-Connection PHP : 
 <?php
+
+# BDD : https://phpmyadmin.alwaysdata.com/
+# User : 122417_root
+#
+# Mot de Passe : password
+#
+# Connection PHP : 
+
 try{
 	$bdd = new PDO('mysql:host=mysql-fitbitproject.alwaysdata.net;dbname=fitbitproject_bdd','122417_root','password');
 	$bdd->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);

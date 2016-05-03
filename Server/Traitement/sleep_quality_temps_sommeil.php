d4d64434c27790ac69ef9e1737c3e3f6e5a5ab39
Cédric Prezelin <cedric.prezelin@outlook.com>
2016-05-03 15:18:16 +0200

    Refactor parse_csv -> include

--- Server/Traitement/sleep_quality_temps_sommeil.php
+++ Server/Traitement/sleep_quality_temps_sommeil.php
@@ -1,7 +1,17 @@
 <?php
 
 // $dataSet is available with tih include
-include 'parse_csv.php';
+include 'include.php';
 
-
-
+// Get imc by the last day until the nbr_jour given by parameter
+function get_sleeping_time($dataset, $nbr_days = 20) {
+    $index_imc = 3;
+    $column_imc = $dataset[0][$index_imc];
+    $lenght_dataset = count($dataset)-1;
+    $imc = array();
+    for ($i =$lenght_dataset; $i > $lenght_dataset-$nbr_days; $i--){
+        $daily_imc = $dataset[$i][$column_imc];
+        $imc[] = $daily_imc;
+    }
+    return $imc;
+}

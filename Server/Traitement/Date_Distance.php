170ddc478c47d58d982f7f24b5e1b02fc07bf8d2
Cédric Prezelin <cedric.prezelin@outlook.com>
2016-05-04 13:49:13 +0200

    finish feeding to database and cleaning

--- Server/Traitement/Date_Distance.php
+++ Server/Traitement/Date_Distance.php
@@ -18,9 +18,8 @@ function feed_bdd_distance($activity, $table, $column)
     $activity_column = meanDateColumnHeadings($dataSet, $activity);
     $activity_column = $activity_column[$column];
     for ($i = 0; $i < count($activity_column); $i++)
-        $bdd->query('INSERT INTO '.$table.' (date, value) VALUES ("' . $activity_column[$i]["date"] . '", "'.$activity_column[$i][$activity].'")');
+        $bdd->query('INSERT INTO '.$table.' (date, steps) VALUES ("' . $activity_column[$i]["date"] . '", "'.$activity_column[$i][$activity].'")');
 }
-
 
 /*
  * DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK
@@ -30,4 +29,11 @@ function feed_bdd_distance($activity, $table, $column)
  * DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK
  * feed_bdd_distance("distance", "kmMonth", "month");
  * DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK
-*/
+ * feed_bdd_distance("steps", "StepsDay", "day");
+ * DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK
+ * feed_bdd_distance("steps", "StepsWeek", "week");
+ * DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK
+ * feed_bdd_distance("steps", "StepsMonth", "month");
+ * DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK | DO NOT RUN THIS HOOK
+ * */
+
